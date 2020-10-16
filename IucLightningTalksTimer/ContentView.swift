c65a520eaed445e69d3875f7a6515ff60dd7aab1
Liang Hai <lianghai@gmail.com>
2020-10-17 03:34:01 +0800

    Initial Commit

---
+++ IucLightningTalksTimer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  IucLightningTalksTimer
+//
+//  Created by 梁海 on 10/17/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ IucLightningTalksTimer/IucLightningTalksTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  IucLightningTalksTimerApp.swift
+//  IucLightningTalksTimer
+//
+//  Created by 梁海 on 10/17/20.
+//
+
+import SwiftUI
+
+@main
+struct IucLightningTalksTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
